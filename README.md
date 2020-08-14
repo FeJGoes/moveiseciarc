51a3ef82467e4eb3d4574106608a148a8c307873
Felipe José de Góes <felipej.tecnologia@supersoft.com.br>
2020-08-13 21:57:16 -0300

    Começo da home

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Moveis&Cia 
 
-Site institucional e administrativo loja de móveis 
+Site institucional e administrativo loja de móveis
+
+# Contruído com 
+
+- Laravel 7.0
+- Uikit 3.5
 
 # Layout UI 
-[ Layout Design XD ](https://xd.adobe.com/view/192b8faa-1b88-43d1-67d1-617afae04992-6f1a/)
+
+[Layout Design XD](https://xd.adobe.com/view/192b8faa-1b88-43d1-67d1-617afae04992-6f1a/)
+[Layout Design XD](https://xd.adobe.com/view/6eeb42c8-fece-40ba-83bd-07e3d9b31a80-3bb8/)
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,52 @@
 @section('title', 'Bem Vindo! Móveis&Cia')
 
 @section('content')
-    {{-- <div uk-alert>
-        <a class="uk-alert-close" uk-close></a>
-        <h3>Notice</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    </div> --}}
+<div class="uk-height-large uk-flex uk-flex-left uk-flex-middle uk-background-cover uk-light" data-src="{{ asset('images/home.png') }}" uk-img>
+    <div class="uk-margin-xlarge-left uk-flex uk-flex-column uk-width-1-4">
+        <img src="{{ asset('images/logo-branco.png') }}" alt="logo" width="60">
+        <hr>
+        <p>Móveis e eletrodomésticos novos e usados. Aceitamos seu usado como base de troca em qualquer produto ou serviço.</p>
+        <small>Conheça melhor nossa empresa</small>
+        <div class="uk-flex uk-flex-left uk-margin ">
+            <button class="uk-button uk-button-small btn-yellow">Empresa</button>
+        </div>
+    </div>
+</div>
+
+<div>
+    <h3>Catálogos</h3>
+    <div class="uk-flex uk-flex-center">
+        <figure class="uk-margin-small">
+            <img src="" alt="novos">
+            <h4>Novos</h4>
+        </figure>
+
+        <figure class="uk-margin-small">
+            <img src="" alt="usados">
+            <h4>Usados</h4>
+        </figure>
+
+        <figure class="uk-margin-small">
+            <img src="" alt="modelados">
+            <h4>Modelados</h4>
+        </figure>
+    </div>
+
+    <p>Hoje em dia, ter uma catálogo online é imprescindível. Você pode alcançar mais clientes, mais rapidamente e a qualquer momento. Os benefícios de ter seu catálogo sempre disponível online são infinitos</p>
+
+    <small>Veja mais</small>
+    <div class="uk-flex uk-flex-left uk-margin ">
+        <button class="uk-button uk-button-small btn-yellow">Catálogos</button>
+    </div>
+</div>
+
+<div>
+    <h3>Contato</h3>
+    <p>Entre em contato com a gente, esclareça suas dúvidas e confira como é nosso trabalho de perto, Diversos tipos de customização e móveis perfeitos para você, é só clicar abaixo!</p>
+
+    <small>Veja mais</small>
+    <div class="uk-flex uk-flex-left uk-margin ">
+        <button class="uk-button uk-button-small btn-yellow">contato</button>
+    </div>
+</div>
 @endsection
